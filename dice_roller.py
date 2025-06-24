d2d88f4f2f29634de438e53b9aa07d55ec214688
LingamJohnEthan29 <161198261+LingamJohnEthan29@users.noreply.github.com>
2025-06-24 13:53:49 +0530

    Add files via upload

---
+++ dice_roller.py
@@ -0,0 +1,13 @@
+import random
+import time 
+def dice_roll():
+    roll = random.randint(1,6)
+    print("Rolling")
+    t = 3
+    while t > 0:
+        print( "... "+str(t))
+        t -=1
+        time.sleep(1)
+    print("Number gotten, ",roll)
+
+dice_roll()
